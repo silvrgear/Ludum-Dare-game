add15c861d56355790c2cc803e8321f95cec87bb
Syarr14 <60078863+Syarr14@users.noreply.github.com>
2020-10-05 12:40:01 +0800

    Update
    -slime particles when they collide with each other or walls (can be remove)

--- script/Slime.gd
+++ script/Slime.gd
@@ -7,3 +7,8 @@ func killed():
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "pop":
 		self.queue_free()
+
+
+func _on_Slime_body_entered(body):
+	$particles.restart()
+	pass # Replace with function body.
